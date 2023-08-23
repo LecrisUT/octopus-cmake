de8fe7bdfa8a310bf4231dc3877f9b8a29a34030
Cristian Le <cristian.le@mpsd.mpg.de>
2023-08-23 11:57:01 +0200

    Initialize

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+project = 'Octopus with CMake'
+copyright = '2023, Cristian Le'
+author = 'Cristian Le'
+
+extensions = [
+    "myst_parser",
+    "sphinx_design",
+    "sphinx_togglebutton",
+]
+
+templates_path = []
+exclude_patterns = [
+    'build',
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    "README.md",
+]
+source_suffix = [".md"]
+
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+myst_enable_extensions = [
+    "tasklist",
+    "colon_fence",
+]
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Octopus with CMake
+
+
